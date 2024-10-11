0f3fb1bed0891daa14b479715c145fb50047dcc6
plusgut <github@webentwickler2-0.de>
2024-10-11 15:51:12 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+            packageName = "anibar";
+        in {
+          defaultPackage = self.packages.${system}.${packageName};
+
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.zig
+            ];
+          };
+        }
+      );
+}
+
